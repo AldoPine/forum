4511c44be1c6c26abe6d32fdcb7516980a1e786c
aldopine <aldopineda_m@yahoo.com.mx>
2016-03-16 13:36:06 -0600

    Add Destroy and Edit Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :find_post, only:[:show, :edit, :update, :destroy]
 
   def index
   end
@@ -17,8 +18,24 @@ class PostsController < ApplicationController
       end
   end
 
-  def show
+  def find_post
     @post = Post.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to root_path
   end
 
 private
