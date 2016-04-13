5e473b88f2a56455c0965cc2ac090360c7d8e55f
aldopine <aldopineda_m@yahoo.com.mx>
2016-04-13 15:54:03 -0500

    Add comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160317220831) do
+ActiveRecord::Schema.define(version: 20160401215543) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "comment"
+    t.integer  "post_id"
+    t.string   "user"
+    t.string   "references"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
